884842a815d06d30296f800fb046cc3a2eca2069
Laboratorio Informatica Tijuca <Labinfotj@LABUVA.EDU>
2019-03-27 11:23:38 -0300

    Questão 1 - Fibonacci Array

---
+++ Radix_Challenge/PrimeiraQuestao/FibonacciSequence.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraQuestao
+{
+    public class FibonacciSequence
+    {
+        private List<long> _seqFibo;
+        public int MaiorTermo { get; } //Propriedade readonly para definir o maior termo de Fibonacci que a lista atingirá. 
+
+        public FibonacciSequence()
+        {
+            MaiorTermo = 60;
+            CreateSequence();
+        }
+
+        //Método que recebe o termo desejado como parâmetro e retorna o valor referente a ele (lança uma exceção caso o termo não seja entre 0 e 60).
+        public long GetItem(int indice)
+        {
+            if(indice < 0 || indice > 60)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _seqFibo[indice];
+        }
+
+      
+        //Cria e implementa a lista _seqFibo até o termo 60 da sequência de Fibonacci. 
+        private void CreateSequence()
+        {
+            _seqFibo = new List<long>();
+            long item, ant1, ant2;
+
+            for (int i = 0; i <= MaiorTermo; i++)
+            {
+                if (i == 0)
+                {
+                    _seqFibo.Add(0);
+                    continue;
+                }
+
+                if (i == 1 || i == 2)
+                {
+                    _seqFibo.Add(1);
+                    continue;
+                }
+
+                ant1 = _seqFibo[i - 1];
+                ant2 = _seqFibo[i - 2];
+
+                item = ant1 + ant2;
+
+                _seqFibo.Add(item);
+            }
+        }
+    }
+}
---
+++ Radix_Challenge/PrimeiraQuestao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraQuestao
+{
+    
+    class Program
+    {
+        /* 
+         * O método Main é responsável por definir quantos testes serão feitos, instanciar a classe FibonacciSequence (onde toda lógica do problema foi implementada) e tratar
+         *  a eventual exceção que seu método poderá lançar.
+         */
+        static void Main(string[] args)
+        {
+            int qtdTestes, termo;
+            long resultado;
+            var fibo = new FibonacciSequence();
+
+            Console.WriteLine("Digite quantos testes serão feitos.");
+            qtdTestes = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < qtdTestes; i++)
+            {
+                Console.WriteLine("Digite o enésimo termo que deseja buscar.");
+                termo = Int32.Parse(Console.ReadLine());
+
+                try
+                {
+                    resultado = fibo.GetItem(termo);
+                    Console.WriteLine($"fibo({termo}) = {resultado}");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Só é permitido buscar do termo 0 ao 60 da sequência de Fibonacci.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
