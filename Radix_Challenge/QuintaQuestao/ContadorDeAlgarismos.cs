98e3ea373211c3c955948da95bd9cefc1e4bb5ae
Eric Martins <ericmartins.martins@gmail.com>
2019-03-28 23:42:45 -0300

    Questao 5 - Digits Count

---
+++ Radix_Challenge/QuintaQuestao/ContadorDeAlgarismos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace QuintaQuestao
+{
+    public class ContadorDeAlgarismos
+    {
+        private int[] _qtdAlgarismos;
+
+        public ContadorDeAlgarismos()
+        {
+            /*
+             * array _QtdAlgarismos irá manter na posição i a quantidade de vezes que 
+             * o algarismo i aparece dentro do intervalo.
+             */
+            _qtdAlgarismos = new int[10]; 
+        }
+
+        public void Contar(int a, int b)
+        { 
+            //Garante que a seja menor que b.
+            if(a > b)
+            {
+                int aux = a;
+                a = b;
+                b = aux;
+            }
+            
+            //Percorre o intervalo (a, b)
+            for(int i = a; i <= b; i++)
+            {
+                char[] array = i.ToString().ToCharArray(); //tranforma o numero num array de char para manipularmos todos os algarismos.
+                Implementar(array);
+            }
+        }
+
+        //recebe o array referente a um dos numeros do intervalo e incrementa _qtdAlgarismos referente os algarismos encontrados.
+        private void Implementar(char[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int numero = Int32.Parse(array[i].ToString());
+                _qtdAlgarismos[numero]++;
+            }
+        }
+
+        //Imprime na tela o array _qtdAlgarismos expondo o resultado.
+        public void ImprimirResultado()
+        {
+            Console.WriteLine();
+
+            foreach (var item in _qtdAlgarismos)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            ZerarContador();
+        }
+
+        //Zera todas as posições de _qtdAlgarismos para ser usado nos próximos intervalos.
+        private void ZerarContador()
+        {
+            for(int i = 0; i < _qtdAlgarismos.Length; i++)
+            {
+                _qtdAlgarismos[i] = 0;
+            }
+        }
+    }
+}
--- Radix_Challenge/QuintaQuestao/Program.cs
+++ Radix_Challenge/QuintaQuestao/Program.cs
@@ -10,10 +10,39 @@ namespace QuintaQuestao
     {
         static void Main(string[] args)
         {
-            string s = "palavra";
+            int a, b, qtdTestes;
+            string[] linha = new string[2];
+            var Contador = new ContadorDeAlgarismos(); //Instância da classe responsável por realizar os testes.
 
-            Console.WriteLine(s.Substring(0, 5));
+            Console.WriteLine("Digite a quantidade de testes que serão feitos.");
+            qtdTestes = Int32.Parse(Console.ReadLine());
 
+            for (int i = 0; i < qtdTestes; i++)
+            {
+                Console.WriteLine("Digite um intervalo de números inteiros " +
+                                  "entre 1 e 1000000 separados por um espaço.");
+                linha = Console.ReadLine().Split(' ');
+
+                //Exceção é lançada caso as entradas não sejam números.
+                try
+                {
+                    a = Int32.Parse(linha[0]);
+                    b = Int32.Parse(linha[1]);
+                    
+                    // Os números precisam estar entre 1 e 1000000 inclusive.
+                    if (a < 1 || a > 1000000 || b < 1 || b > 1000000)
+                    {
+                        Console.WriteLine("\nOs números devem estar entra 1 e 1000000.\n");
+                        continue;
+                    }
+                    Contador.Contar(a, b); //Chama o método responsável por realizar a contagem de algarismos.
+                    Contador.ImprimirResultado(); //Imprime resultado esperado. 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nEntrada Inválida!\n");
+                }
+            }
             Console.ReadLine();
         }
     }
